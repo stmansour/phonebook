27473f375d55bebe27fe31e4d309ac9ddea47c3f
Steve Mansour <sman@stevemansour.com>
2015-12-24 13:26:07 -0800

    fix to file upload issue

--- savePersonDetails.go
+++ savePersonDetails.go
@@ -4,12 +4,95 @@ import (
 	"crypto/sha512"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
+
+func uploadFileCopy(from *multipart.File, toname string) error {
+	to, err := os.Create(toname)
+	if err != nil {
+		ulog("uploadImageFile: Error on os.Create(%s) -- err=%v\n", toname, err)
+		return err
+	}
+	defer to.Close()
+	_, err = io.Copy(to, *from)
+	if err != nil {
+		ulog("savePersonDetailsHandler: Error writing picture file: %v\n", err)
+	} else {
+		ulog("File uploaded successfully to %s\n", toname)
+	}
+	return err
+}
+
+// uploadImageFile handles the uploading of a user's picture file and its
+// placement in the pictures directory.
+//
+// Params
+// usrfname - name of the file on the user's local system
+// usrfile - the open file from the form return in the user's browser
+// uid - uid of the user for whom the image applies
+//
+// Returns:  err from any os file function
+func uploadImageFile(usrfname string, usrfile *multipart.File, uid int) error {
+	// use the same filetype for the final filename
+	ftype := filepath.Ext(usrfname)
+	// ulog("user file type: %s\n", ftype)
+
+	// the file name we'll use for this user's picture...
+	picturefilename := fmt.Sprintf("pictures/%d%s", uid, ftype)
+	// ulog("picturefilename: %s\n", picturefilename)
+
+	//  delete old tmp file if exists:
+	tmpFile := fmt.Sprintf("pictures/%d.tmp", uid)
+	// ulog("tmpFile to delete if exists: %s\n", tmpFile)
+
+	finfo, err := os.Stat(tmpFile)
+	if os.IsNotExist(err) {
+		ulog("%s was not found. Nothing to delete\n", tmpFile)
+	} else {
+		ulog("os.Stat(%s) returns:  err=%v,  finfo=%#v\n", tmpFile, err, finfo)
+		err = os.Remove(tmpFile)
+		ulog("os.Remove(%s) returns err=%v\n", tmpFile, err)
+	}
+
+	// copy the requested file to "<uid>.tmp"
+	err = uploadFileCopy(usrfile, tmpFile)
+	if nil != err {
+		ulog("uploadFileCopy returned error: %v\n", err)
+		return err
+	}
+
+	// see if there are any files that match the old filename MINUS the filetype...
+	m, err := filepath.Glob(fmt.Sprintf("./pictures/%d.*", uid))
+	if nil != err {
+		ulog("filepath.Glob returned error: %v\n", err)
+		return err
+	}
+	// ulog("filepath.Glob returned the following matches: %v\n", m)
+	for i := 0; i < len(m); i++ {
+		if filepath.Ext(m[i]) != ".tmp" {
+			ulog("removing %s\n", m[i])
+			err = os.Remove(m[i])
+			if nil != err {
+				ulog("error removing file: %s  err = %v\n", m[i], err)
+				return err
+			}
+		}
+	}
+
+	// now move our newly uploaded picture into its proper name...
+	err = os.Rename(tmpFile, picturefilename)
+	if nil != err {
+		ulog("os.Rename(%s,%s):  err = %v\n", tmpFile, picturefilename, err)
+		return err
+	}
+
+	return nil
+}
 
 func savePersonDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	var sess *session
@@ -50,7 +133,8 @@ func savePersonDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/search/", http.StatusFound)
 		return
 	}
-
+	d.UID = uid
+	adminReadDetails(&d) //read current data
 	action := strings.ToLower(r.FormValue("action"))
 	if "save" == action {
 		d.PreferredName = r.FormValue("PreferredName")
@@ -66,7 +150,11 @@ func savePersonDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		d.HomePostalCode = r.FormValue("HomePostalCode")
 		d.HomeCountry = r.FormValue("HomeCountry")
 
-		sess.Firstname = d.PreferredName
+		if 0 == len(d.PreferredName) {
+			sess.Firstname = d.FirstName
+		} else {
+			sess.Firstname = d.PreferredName
+		}
 
 		//=================================================================
 		// handle image
@@ -75,25 +163,15 @@ func savePersonDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		// fmt.Printf("file: %v, header: %v, err: %v\n", file, header, err)
 		if nil == err {
 			defer file.Close()
-			ftype := filepath.Ext(header.Filename)
-			picturefilename := fmt.Sprintf("pictures/%d%s", uid, ftype)
-			out, err := os.Create(picturefilename)
-			if err != nil {
-				ulog("savePersonDetailsHandler: Unable to create the file for writing. err=%v\n", err)
-			} else {
-				defer out.Close()
-				_, err = io.Copy(out, file)
-				if err != nil {
-					ulog("savePersonDetailsHandler: Error writing picture file: %v\n", err)
-				} else {
-					ulog("Picture file %s uploaded successfully to %s\n", header.Filename, picturefilename)
-				}
+			err = uploadImageFile(header.Filename, &file, uid)
+			if nil != err {
+				ulog("uploadImageFile returned error: %v\n", err)
 			}
 			sess.ImageURL = getImageFilename(uid)
 		}
 
 		//=================================================================
-		// handle image
+		// Do the update
 		//=================================================================
 		update, err := Phonebook.db.Prepare("update people set PreferredName=?,PrimaryEmail=?,OfficePhone=?,CellPhone=?," +
 			"EmergencyContactName=?,EmergencyContactPhone=?," +
